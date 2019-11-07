b0787dd58ca2189c3e3e5e78459c509b5a2db738
Luana Faria Lane <luanasfff@gmail.com>
2019-11-07 00:30:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,11 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
